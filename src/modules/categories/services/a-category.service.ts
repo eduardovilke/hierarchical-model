a46a770cf86ecf09b1dc25e5fda9ab7f00e8b0e5
Eduardo Vilke <eduardo.svilke@gmail.com>
2025-03-04 19:01:11 -0300

    fix: create on adjacency

--- src/modules/categories/services/a-category.service.ts
+++ src/modules/categories/services/a-category.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ACategory } from '../entities/a-category.entity';
 import { Repository } from 'typeorm';
+import { CreateCategoryDTO } from '../create-category.dto';
 
 @Injectable()
 export class ACategoryService {
   constructor(
     @InjectRepository(ACategory)
     private readonly categoryRepository: Repository<ACategory>,
-  ) {}
+  ) { }
 
-  create(data: any) {
-    return this.categoryRepository.save(this.categoryRepository.create(data));
+  create(data: CreateCategoryDTO) {
+    const category = this.categoryRepository.create(data);
+    if (data.parentId) {
+      category.parent = { id: data.parentId } as ACategory;
+    }
+
+    return this.categoryRepository.save(category);
   }
 
   getById(id: string) {
